072d77f56c9a1ea6f58291005897e4e9a4be93ee
SeftZzz <yerblues6@gmail.com>
2025-08-24 18:22:42 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Universe Of Gamers NFT',
+  webDir: 'www'
+};
+
+export default config;
