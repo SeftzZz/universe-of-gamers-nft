fd43f3ed93961c4b97052453377bd8d4876a26cd
SeftZzz <yerblues6@gmail.com>
2025-10-02 07:20:59 +0700

    update build.gradle

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  baseUrl: 'http://localhost:3000',
-  // apiUrl: 'https://api.universeofgamers.io/api',
+  // apiUrl: 'http://localhost:3000/api',
+  // baseUrl: 'http://localhost:3000',
+  apiUrl: 'https://api.universeofgamers.io/api',
+  baseUrl: 'https://api.universeofgamers.io',
   rpcUrl: 'https://mainnet.helius-rpc.com/?api-key=99344f8f-e269-4d69-b838-675fad917aa0',
   programId: 'uogCiN3JxzZgYRDwuefPjx2Hngsf1p3bA6HNAVqkJgq'
 };
